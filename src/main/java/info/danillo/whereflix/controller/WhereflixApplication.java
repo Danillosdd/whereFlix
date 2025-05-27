e116707a6ceeb442068db310734a036204556a0d
Danillo.sdd <danillo.sdd@hotmail.com>
2025-05-26 22:35:41 -0300

    Update

---
+++ src/main/java/info/danillo/whereflix/controller/WhereflixApplication.java
@@ -0,0 +1,13 @@
+package info.danillo.whereflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/danillo/whereflix/WhereflixApplicationTests.java
@@ -0,0 +1,13 @@
+package info.danillo.whereflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
