6c1163ef3aa6a05eca2b2755137e843f3e36fd47
Danillo.sdd <danillo.sdd@hotmail.com>
2025-05-26 22:57:42 -0300

    Update

--- src/main/java/info/danillo/whereflix/controller/WhereflixApplication.java
+++ src/main/java/info/danillo/whereflix/controller/WhereflixApplication.java
@@ -1,4 +1,4 @@
-package info.danillo.whereflix;
+package info.danillo.whereflix.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
