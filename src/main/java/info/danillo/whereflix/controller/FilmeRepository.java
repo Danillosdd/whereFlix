051f9a53f1451c6b552e01090ca8eb7f64e56a91
danillo.sdd <danillo.sdd@hotmail.com>
2025-06-09 22:59:02 -0300

    Update

--- src/main/java/info/danillo/whereflix/controller/FilmeRepository.java
+++ src/main/java/info/danillo/whereflix/controller/FilmeRepository.java
@@ -7,34 +7,34 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 /**
- * Repositório responsável por realizar operações de acesso a dados para a entidade Aluno.
+ * Repositório responsável por realizar operações de acesso a dados para a entidade filme.
  * Extende JpaRepository para fornecer métodos padrão de CRUD e permite a definição de consultas personalizadas.
  */
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
 
     /**
-     * Busca alunos cujo nome contenha o texto fornecido, ignorando maiúsculas e minúsculas.
+     * Busca filmes cujo nome contenha o texto fornecido, ignorando maiúsculas e minúsculas.
      * A consulta utiliza JPQL para realizar a busca com o operador LIKE.
      *
-     * @param nome Texto a ser pesquisado no nome dos alunos.
-     * @return Lista de alunos que correspondem ao critério de busca.
+     * @param nome Texto a ser pesquisado no nome dos filmes.
+     * @return Lista de filmes que correspondem ao critério de busca.
      */
-    @Query("SELECT a FROM Aluno a WHERE LOWER(a.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    @Query("SELECT a FROM Filme a WHERE LOWER(a.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
     List<Filme> buscarPorNome(@Param("nome") String nome);
 
     /**
-     * Verifica se existe algum aluno com a matrícula fornecida no banco de dados.
+     * Verifica se existe algum filme com a matrícula fornecida no banco de dados.
      *
      * @param matricula Número de matrícula a ser verificado.
-     * @return true se existir um aluno com a matrícula fornecida, caso contrário false.
+     * @return true se existir um filme com a matrícula fornecida, caso contrário false.
      */
     boolean existsByMatricula(Integer matricula);
 
     /**
-     * Busca e retorna um aluno com a matrícula fornecida no banco de dados.
+     * Busca e retorna um filme com a matrícula fornecida no banco de dados.
      *
-     * @param matricula Número de matrícula do aluno a ser buscado.
-     * @return Objeto Aluno correspondente à matrícula fornecida, ou null se não encontrado.
+     * @param matricula Número de matrícula do filme a ser buscado.
+     * @return Objeto Filme correspondente à matrícula fornecida, ou null se não encontrado.
      */
     Filme findByMatricula(Integer matricula);
 }
