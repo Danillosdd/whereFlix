7ad77d04dbc406f6ad9197d84ab1c7f7715ae2c9
danillo.sdd <danillo.sdd@hotmail.com>
2025-06-15 23:27:16 -0300

    Update

--- src/main/java/info/danillo/whereflix/controller/IndexControler.java
+++ src/main/java/info/danillo/whereflix/controller/IndexControler.java
@@ -2,10 +2,8 @@ package info.danillo.whereflix.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-
-import info.danillo.whereflix.repository.TipoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Controlador responsável por gerenciar as operações relacionadas à entidade
