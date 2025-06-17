014d77dc496572c71c1793fb343464cbd40964cc
danillo.sdd <danillo.sdd@hotmail.com>
2025-06-17 00:13:53 -0300

    Update

--- src/main/java/info/danillo/whereflix/controller/WhereflixRestController.java
+++ src/main/java/info/danillo/whereflix/controller/WhereflixRestController.java
@@ -3,6 +3,9 @@ package info.danillo.whereflix.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class LancamentoRestController {
+public class WhereflixRestController {
 
     @Autowired
     private IndexRepository indexRepository;
+
+    @Autowired
+    private FilmeRepository filmeRepository;
 
     @GetMapping("/lancamentos")
     public List<Filme> getLancamentos(@RequestParam(required = false) String tipo) {
@@ -33,5 +39,14 @@ public class LancamentoRestController {
         } else {
             return indexRepository.findTop8ByTipoNomeOrderByAvaliacaoDesc(tipo);
         }
+    }
+
+    @GetMapping("/filmes-paginados")
+    public Page<Filme> getFilmesPaginados(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return filmeRepository.findAll(pageable);
     }
 }
